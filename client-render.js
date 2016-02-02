304a94db805208760457662379bf7d831d6915a0
chriskohlbrenner <c.kohlbrenner@gmail.com>
2016-02-02 08:43:30 -0500

    cleanup bad imports

--- client-render.js
+++ client-render.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import { Router } from 'react-router';
+import ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
 
 import { routes } from './routes';
 
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-
 ReactDOM.render(
-  <Router routes={routes} history={createBrowserHistory()} />,
+  <Router routes={routes} history={browserHistory} />,
   document.getElementById('app')
 )
